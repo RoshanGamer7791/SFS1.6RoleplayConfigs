3aec504258e2fed191ec14b1cc5f929ae798cc7e
Roshan <mdtahanchy@gmail.com>
2026-03-01 12:17:20 +0000

    Added a test rp changer

--- nodes.py
+++ nodes.py
@@ -6,3 +6,8 @@ root = None
 mainframe = tk.Frame(root, bg="black", width=1200, height=800)
 mainframe.grid_propagate(False)
 mainframe.grid()
+
+def rpchanger():
+    rpchangerwin = tk.Toplevel(root)
+    rpchangerwin.title("Research Point Changer")
+    rpchangerwin.geometry("500x500")
