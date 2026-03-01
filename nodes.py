3150ab28507c585e730f5876421410f786d93ac5
Roshan <mdtahanchy@gmail.com>
2026-03-01 12:20:06 +0000

    Added the button cuz i forgor to

--- nodes.py
+++ nodes.py
@@ -11,3 +11,6 @@ def rpchanger():
     rpchangerwin = tk.Toplevel(root)
     rpchangerwin.title("Research Point Changer")
     rpchangerwin.geometry("500x500")
+
+rpchangerbtn = tk.Button(root, bg="black", fg="white", font=("Segoe UI", 15, "bold"), command=rpchanger)
+rpchangerbtn.grid(row=0, column=0)
